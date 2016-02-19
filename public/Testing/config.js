b00eb072ff33931d74a199ce5f32ee3cf33f0bbf
Duy Vu <duyvu.mailbox@yahoo.com>
2016-02-19 02:20:05 -0500

    adding main controllers and views layout, header added

---
+++ public/Testing/config.js
@@ -0,0 +1,15 @@
+/**
+ * Created by duyvu on 2/19/2016.
+ */
+(function(){
+    angular
+        .module("FormBuilderApp")
+        .config(Configure);
+
+    function Configure($routeProvider) {
+        $routeProvider
+            //.when("/home",{
+            //    templateUrl: "home.html"
+            //})
+    }
+})();
